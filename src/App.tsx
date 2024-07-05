fe2532ed828f7402d053a511a883a3ac28c79889
Kadeem Cherman (Kassie) <86675041+KassieSanisclaws@users.noreply.github.com>
2024-07-04 20:27:27 -0400

    initial setup of table and components using mui and data grid pro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Box, Typography, Button } from '@mui/material'
+import { DataGridPro } from '@mui/x-data-grid-pro'
+import { useDemoData } from '@mui/x-data-grid-generator'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { data } = useDemoData({ dataSet: 'Commodity', rowLength: 100000, editable: true });
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ height: 520, width: "100%" }}>
+
+      <Typography
+          variant="h3"
+          component="h3"
+          sx={{textAlign: "center", mt: 3, mb: 3 }}>
+          My Data Table Example
+      </Typography>
+      
+      <DataGridPro  
+          {...data}
+          loading={data.rows.length === 0}
+          rowHeight={38}
+          checkboxSelection
+          disableRowSelectionOnClick>
+            
+          </DataGridPro>
+     
+     
+    </Box>
   )
 }
 
