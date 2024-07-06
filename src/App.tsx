720968b288799f8856438cb94932e9ebe992e5f0
Kadeem Cherman (Kassie) <86675041+KassieSanisclaws@users.noreply.github.com>
2024-07-05 20:14:50 -0400

    updated changes to data grid inprogress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react'
-import { Box, Typography, Button } from '@mui/material'
+import { Box, Typography, Button, FormControl, InputLabel, SelectChangeEvent, MenuItem, NativeSelect, ButtonGroup } from '@mui/material'
 import { DataGrid, GridColDef, GridValidRowModel } from '@mui/x-data-grid'
 import { useDemoData } from '@mui/x-data-grid-generator'
 import './App.css'
@@ -7,6 +7,22 @@ import './App.css'
 function App() {
   const { data } = useDemoData({ dataSet: 'Commodity', rowLength: 100000, editable: true });
   const [pageSize, setPageSize] = useState(100);
+  const [alternateMedia, setAlternateMedia] = useState<string>("1");
+
+  const buttons = [
+    <Button 
+         sx={{ border: "1px solid grey" }}
+         >
+          -
+    </Button>,
+    <Button variant="contained" color="secondary" disabled>1</Button>,
+    <Button variant="contained" color="secondary">+</Button>,
+  ];
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAlternateMedia(e.target.value);
+  };
 
   const columns: readonly GridColDef<GridValidRowModel>[] = useMemo(() => [
     { field: "compound", headerName: "Compound", width: 200 },
@@ -20,9 +36,9 @@ function App() {
     {
       field: "details",
       headerName: "Details",
-      width: 100,
+      width: 90,
       renderCell: () => 
-        <Button variant="text" color="primary" sx={{ borderRadius: "50px",  maxHeight: "25px", border: ".2px solid grey" }}>
+        <Button variant="text" color="primary" sx={{ borderRadius: "50px",  maxHeight: "25px", border: "2px solid grey", }}>
           <Typography 
                variant="subtitle2" 
                color="primary"
@@ -35,14 +51,33 @@ function App() {
     {
       field: "alternateMedia",
       headerName: "Alternate Media",
-      width: 80,
+      width: 115,
       type: "singleSelect",
       valueOptions: Array.from({ length: 10}, (_, i) => (i + 1).toString()),
       editable: true,
-      renderCell: (params) => (
-        <Button variant="contained" color="primary" sx={{ }}>
-          {params.value}
-        </Button>
+      renderCell: () => (
+        <Box sx={{ borderRadius: "50px", maxHeight: "23px", margin: "12px 0 0 21px", width: "50px", border: "2px solid grey", }}>
+          <FormControl variant='standard'>
+             <NativeSelect
+                id="alternateMedia"
+                value={alternateMedia}
+                // label={alternateMedia}
+                onChange={handleChange}
+                sx={{ padding: "0 0 0 8px", mt: -.5 }} 
+                disableUnderline 
+                 >
+               {Array.from({ length: 10 }, (_, i) => (
+                <option 
+                    key={i + 1} 
+                    value={(i + 1).toString()}
+                    style={{ textAlign: "center", padding: "8px", }}
+                    >
+                      {i + 1}
+                </option>
+              ))}
+             </NativeSelect>
+             </FormControl>
+        </Box>
       )
     },
     {
@@ -63,14 +98,19 @@ function App() {
     {
       field: "quantity",
       headerName: "Quantity",
-      width: 150,
-      
+      width: 145,
+      renderCell: () => 
+        <Box sx={{ maxWidth: "40vw", overflow: "hidden" }}>
+           <ButtonGroup size="small" sx={{ width: "100%"}}>
+             {buttons}
+           </ButtonGroup>
+        </Box>
     },
     {
       field: "button",
       headerName: "",
       width: 100,
-      renderCell: () => <Button variant="contained" color="primary">Add</Button>
+      renderCell: () => <Button variant="contained" color="error">Add</Button>
     },
   ], []);
   
@@ -91,10 +131,9 @@ function App() {
           paginationModel={{ pageSize, page: 0}}
           onPaginationModelChange={(newModel) => setPageSize(newModel.pageSize)}
           pageSizeOptions={[5, 10, 25, 50, 100]}
-          
-          >
+          />
             
-          </DataGrid>
+         
      
      
     </Box>
